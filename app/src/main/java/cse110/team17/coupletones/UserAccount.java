edc79723a6ca6c90214816930642ebddc89d4ef6
Tony Lim <atomictheorist@gmail.com>
2016-05-08 14:50:31 -0700

    ZZZQ-22 - Add user class as container for favorite places, partner's phone and names

---
+++ app/src/main/java/cse110/team17/coupletones/UserAccount.java
@@ -0,0 +1,80 @@
+package cse110.team17.coupletones;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tony Lim on 5/8/16.
+ * UserAccoutn is a class to represent informations about the user of the App. These include the
+ * name of the user, list of locations, and partner's phone number, regID or email
+ */
+public class UserAccount {
+
+    private String mUsername;
+    private String mUserEmail;
+
+    private boolean hasPartner;
+    private String mPartnerEmail;
+    private String mPartnerRegId;
+    private Integer mPartnerPhone;
+
+    /**
+     * favorite places that contains Markers.
+     * data is public for easier retrieval
+     */
+    public ArrayList<Marker> mFavoritePlaces;
+
+    public UserAccount(){
+        hasPartner = false;
+    }
+
+
+    /**
+     * add partner through phone number
+     * @param partnerPhone : partner's phone number
+     * @return boolean to represent whether a partner already added or not
+     */
+    public boolean addPartner(final Integer partnerPhone)
+    {
+        if( !hasPartner) return false;
+        hasPartner = true;
+        mPartnerPhone = partnerPhone;
+        return true;
+    }
+
+    /**
+     * Add new favorite place to user's
+     * @param favoritePlace : Marker for favorite place
+     */
+    public void addFavoritePlace(Marker favoritePlace)
+    {
+        mFavoritePlaces.add(favoritePlace);
+    }
+
+
+    /**
+     * Getter for member variables
+     */
+    public Integer getPartnerPhone() {
+        return mPartnerPhone;
+    }
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public String getUserEmail() {
+        return mUserEmail;
+    }
+
+    public String getPartnerEmail() {
+        return mPartnerEmail;
+    }
+
+    public String getPartnerRegId() {
+        return mPartnerRegId;
+    }
+}

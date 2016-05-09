1e5d22085d3dfdbd54b6c31c69182d601fdc1057
Tony Lim <atomictheorist@gmail.com>
2016-05-08 20:56:09 -0700

    Simplify login interface to include only partner's phone

--- app/src/main/java/cse110/team17/coupletones/LoginActivity.java
+++ app/src/main/java/cse110/team17/coupletones/LoginActivity.java
@@ -40,10 +40,6 @@ public class LoginActivity extends AppCompatActivity  {
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -70,11 +66,12 @@ public class LoginActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+//        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         mPhoneView = (AutoCompleteTextView) findViewById(R.id.phone_number);
 
         mUserAccount = new UserAccount();
 
+        // TODO: for now this sign in button is disabled (May be needed for final app)
         mButtonSignIn = (Button) findViewById(R.id.sign_in_button);
         mButtonSignIn.setOnClickListener(new OnClickListener() {
             @Override
@@ -84,7 +81,7 @@ public class LoginActivity extends AppCompatActivity  {
                 } else {
                     mState = Constants.State.REGISTERED;
                     //mEmailView.setHint("Partner's Email");
-                    mEmailView.setText("");
+//                    mEmailView.setText("");
                     //mPhoneView.setHint("Partner's Phone number");
                     mPhoneView.setText("");
                     mButtonSignIn.setEnabled(false);
@@ -94,6 +91,7 @@ public class LoginActivity extends AppCompatActivity  {
             }
         });
 
+        mState = Constants.State.REGISTERED; // Only phone registration for partner is allowed
         mButtonSignPartner = (Button) findViewById(R.id.sign_partner);
         mButtonSignPartner.setOnClickListener(new OnClickListener() {
             @Override
@@ -102,7 +100,7 @@ public class LoginActivity extends AppCompatActivity  {
                     Toast.makeText(LoginActivity.this, "No inputs.", Toast.LENGTH_SHORT).show();
                 } else {
                     mStatePartner = Constants.State.REGISTERED;
-                    mEmailView.setEnabled(false);
+//                    mEmailView.setEnabled(false); // TODO: may needed for final
                     mPhoneView.setEnabled(false);
                     mButtonSignPartner.setEnabled(false);
                     Toast.makeText(LoginActivity.this, "Partner registered", Toast.LENGTH_SHORT).show();
@@ -143,8 +141,8 @@ public class LoginActivity extends AppCompatActivity  {
 //        })
 
         // New button to go to map activity
-        Button mTestMapButton = (Button) findViewById(R.id.test_go_to_map);             assert mTestMapButton != null;
-        mTestMapButton.setEnabled(false);
+//        Button mTestMapButton = (Button) findViewById(R.id.test_go_to_map);             assert mTestMapButton != null;
+//        mTestMapButton.setEnabled(false);
 //        mTestMapButton.setOnClickListener(new OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -163,7 +161,7 @@ public class LoginActivity extends AppCompatActivity  {
         String partnerPhone = mPhoneView.getText().toString();
         Log.d(LOGIN, "parterPhone : " + partnerPhone);
         mUserAccount.addPartner(partnerPhone);
-        mUserAccount.setPartnerEmail(mEmailView.getText().toString());  // NOTE: might be empty
+//        mUserAccount.setPartnerEmail(mEmailView.getText().toString());  // NOTE: might be empty
 
         // TODO: register partner here, need to do all that authentication
         return true;
@@ -173,6 +171,8 @@ public class LoginActivity extends AppCompatActivity  {
      * Function to register the information on user
      */
     private boolean registerUser() {
+        Log.d(LOGIN, "Do no call this function");
+
         String email = mEmailView.getText().toString();
         String phone = mPhoneView.getText().toString();
         Log.d(LOGIN, "email : " + email);
@@ -187,138 +187,5 @@ public class LoginActivity extends AppCompatActivity  {
         return true;
     }
 
-
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-    private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-//        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-//            mPasswordView.setError(getString(R.string.error_invalid_password));
-//            focusView = mPasswordView;
-//            cancel = true;
-//        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-//            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
-        }
-    }
-
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-//                mPasswordView.setError(getString(R.string.error_incorrect_password));
-//                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
 
